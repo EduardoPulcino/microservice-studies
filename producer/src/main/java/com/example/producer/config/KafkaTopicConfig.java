62ee1dae4285f7a092daccd68a65a68651398ff2
EduardoPulcino <eduardo.pulcino@sptech.school>
2025-03-28 17:54:41 -0300

    Add topic config

---
+++ producer/src/main/java/com/example/producer/config/KafkaTopicConfig.java
@@ -0,0 +1,28 @@
+package com.example.producer.config;
+
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaAdmin;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaTopicConfig {
+    @Value(value = "${spring.kafka.bootstrap-servers}")
+    private String bootstrapAdress;
+
+    @Bean
+    public KafkaAdmin kafkaAdmin() {
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAdress);
+        return new KafkaAdmin(configs);
+    }
+
+    public NewTopic topicOrderProcess() {
+        return new NewTopic("orders-process", 2, (short) 1);
+    }
+}
